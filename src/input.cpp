ac053d5b75f74134643019678a5aac437d99055f
code2nvim <code2nvim@gmail.com>
2025-05-17 13:21:14 +0800

    add pointer_event: convert event to string

--- src/input.cpp
+++ src/input.cpp
@@ -20,7 +20,7 @@ auto to_string(const app::event_ptr& event) -> std::string
     case LIBINPUT_EVENT_KEYBOARD_KEY:
         return app::keyboard_event(event);
     case LIBINPUT_EVENT_POINTER_BUTTON:
-        return "pointer";
+        return app::pointer_event(event);
     default:
         return "unknown";
     }
@@ -41,7 +41,7 @@ auto input() -> std::generator<std::string>
     libinput_udev_assign_seat(libinput.get(), "seat0");
     app::pollfd fds = {
         .fd = libinput_get_fd(libinput.get()),
-        .events = 0x001,
+        .events = 0x001, // POLLIN from <poll.h>
         .revents = 0,
     };
     for (;;) {
--- src/input/event.cpp
+++ src/input/event.cpp
@@ -29,4 +29,19 @@ auto keyboard_event(const app::event_ptr& event) -> std::string
     };
 }
 
+auto pointer_event(const app::event_ptr& event) -> std::string
+{
+    auto* pointer = libinput_event_get_pointer_event(event.get());
+    const std::string_view name {
+        libevdev_event_code_get_name(0x01, // EV_KEY <linux/input-event-codes.h>
+            libinput_event_pointer_get_button(pointer)),
+    };
+    switch (libinput_event_pointer_get_button_state(pointer)) {
+    case LIBINPUT_BUTTON_STATE_PRESSED:
+        return std::format("pressed: {}", name.data());
+    case LIBINPUT_BUTTON_STATE_RELEASED:
+        return std::format("released: {}", name.data());
+    };
+}
+
 }
