d8a15c151bc77b8a33d686d80ba75bfe89751a6e
code2nvim <code2nvim@gmail.com>
2025-05-25 01:11:29 +0800

    format xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -14,24 +14,24 @@ add_requires("pkgconf::libinput", { alias = "libinput" })
 add_requires("pkgconf::libevdev", { alias = "libevdev" })
 
 target("yank-keys", function()
-    add_packages("gtkmm", "libudev", "libinput", "libevdev")
-    add_files(
-        "src/input/*.cpp",
-        "src/input.cpp",
-        "src/window/*.cpp",
-        "src/window.cpp",
-        "src/main.cpp"
-    )
+  add_packages("gtkmm", "libudev", "libinput", "libevdev")
+  add_files(
+    "src/input/*.cpp",
+    "src/input.cpp",
+    "src/window/*.cpp",
+    "src/window.cpp",
+    "src/main.cpp"
+  )
 end)
 
 target("test", function()
-    add_tests("input_test")
-    add_packages("libudev", "libinput", "libevdev")
-    add_links("boost_unit_test_framework")
-    add_files(
-        "src/input/*.cpp",
-        "src/input.cpp",
-        "test/input_test.cpp"
-    )
-    add_defines("BOOST_TEST_DYN_LINK")
+  add_tests("input_test")
+  add_packages("libudev", "libinput", "libevdev")
+  add_links("boost_unit_test_framework")
+  add_files(
+    "src/input/*.cpp",
+    "src/input.cpp",
+    "test/input_test.cpp"
+  )
+  add_defines("BOOST_TEST_DYN_LINK")
 end)
